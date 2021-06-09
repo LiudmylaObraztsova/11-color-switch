96b71c69e0b2ae4a7c80324e5c1068a9c9bd39fc
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-06-10 00:33:32 +0300

    Добавляет файлы из черновика

--- js/index.js
+++ js/index.js
@@ -6,3 +6,23 @@ const refs = {
     stopBtn: document.querySelector('[data-action="stop"]'),
     body: document.querySelector('body'),
 }
+
+let changeColorId;
+
+const newRandomColor = () =>
+    refs.bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+
+function clickOnStart() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    changeColorId = setInterval(newRandomColor, 1000);
+}
+
+function clickOnStop() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(changeColorId);
+}
+
+refs.startBtn.addEventListener('click', clickOnStart);
+refs.stopBtn.addEventListener('click', clickOnStop);
