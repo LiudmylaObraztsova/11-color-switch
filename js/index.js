8d8460cd03ab556cb79ccac5b2fda29206a1f07d
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-06-10 00:32:09 +0300

    add import

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './colors.js';
+import randomIntegerFromInterval from './random-integer.js'
+
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+}
